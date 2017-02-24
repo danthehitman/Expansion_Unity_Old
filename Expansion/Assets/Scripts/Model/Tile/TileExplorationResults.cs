547f0e859d48c9bd2b4b987a775aa4ade0c4c33e
Dan Frank <danthehitman@gmail.com>
2017-02-24 14:04:06 -0700

    WIP

--- Expansion/Assets/Scripts/Model/Tile/TileExplorationResults.cs
+++ Expansion/Assets/Scripts/Model/Tile/TileExplorationResults.cs
@@ -1,16 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TileExplorationResults : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+﻿public class TileExplorationResults
+{
+    public Inventory ExplorationInventory { get; set; }
+    public float ElapsedTimeHours { get; set; }
+    
 }
